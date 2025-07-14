972103b796359a03f357685a44e12e9278384bac
lakustovdimka <lakustovdimka@gmail.com>
2025-07-14 15:07:43 +0500

    12345678912

--- main.py
+++ main.py
@@ -22,6 +22,6 @@ if __name__ == "__main__":
             new_password = generate_password(pass_length)
             print(f"Ваш сгенерированный пароль: {new_password}")
     except ValueError:
-        print("Некорректный ввод133454669781527890zxcv. Пожалуйста, введите целое число для длины.")
+        print("Некорректный ввод1334546697812527890zxcv. Пожалуйста, введите целое число для длины.")
 
 
